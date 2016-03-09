d4649260a573e6a0d857e43241aeb672fab64feb
soumyarintu1984 <soumyaranjandas@outlook.com>
2016-03-08 20:23:36 -0600

    R Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions in this file help create and cache a matrix and its inverse
+## so that one does not need to calculate matrix inverse if the matrix
+## has not changed and a matrix inverse is present
 
-## Write a short comment describing this function
+## makeCacheMatrix function: Creates a matrix that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv_matrix <- NULL
+  create_matrix <- function(y){
+    x <<- y
+    inv_matrix <<- NULL
+  }
+  
+  get_matrix <- function() x
+  
+  set_matrix_inv <- function(inv) inv_matrix <<- inv
+  get_matrix_inv <- function() inv_matrix
+  
+  list(create_matrix = create_matrix, get_matrix = get_matrix, 
+       set_matrix_inv = set_matrix_inv, get_matrix_inv = get_matrix_inv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve Function: Takes the output matrix from makeCacheMatrix function
+## and retrieves the inverse of the matrix from its cache, if not, calculate it 
+## and store it in the matrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inv_matrix <- x$get_matrix_inv()
+  if(!is.null(inv_matrix)){
+    message("Fetching Cached Matrix Inverse")
+    return(inv_matrix)
+  }
+  
+  spec_matrix <- x$get_matrix()
+  inv_matrix <- solve(spec_matrix)
+  x$set_matrix_inv(inv_matrix)
+  inv_matrix
+    
 }
